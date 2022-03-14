9a15b67c35a040b0029263e8a7de0fa4ba78a786
DET171 <DET171@users.noreply.github.com>
2022-03-14 06:44:52 +0000

    deploy: 825cf4ecad942aba1545ba8bbb02269b170d8e53

---
+++ _next/static/Gn9bjPR11FDw-EeBGoED_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/757-70eb0ed4b0d0ecec.js","static/chunks/pages/index-b4a858ffeb7dd77f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
