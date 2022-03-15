2a10be9402f17cfb9b877cad79ff5f79b1d67c20
donthaveausername <101548078+donthaveausername@users.noreply.github.com>
2022-03-15 16:23:10 +0800

    Update index.js
    google signin

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ class App extends Component {
 
 	render() {
 		return (
+			<script src="https://apis.google.com/js/platform.js" async defer></script>
+			<meta name="google-signin-client_id" content="554823153885-g6qe6cqvtbrf2oico1m8ag5gkqsqupnm.apps.googleusercontent.com.apps.googleusercontent.com">
 			<>
 				<Container style={{
 					display: 'flex',
@@ -35,6 +37,7 @@ class App extends Component {
 								paddingBottom: '30px',
 							}} />
 							<Button positive fluid>Login</Button>
+							<div class="g-signin2" data-onsuccess="onSignIn"></div>
 						</Container>
 					</Segment>
 				</Container>
